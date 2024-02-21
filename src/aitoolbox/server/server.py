e3675ccb61fc74bb8f0f6d3b5a0fad94f5e2346f
Omri Bloch <omri.bloch@gmail.com>
2024-02-21 21:47:41 +0200

    server, package info, ai-server command

---
+++ src/aitoolbox/server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from aitoolbox.server.state import ConvesationState
+
+
+def main():
+    app = Flask(__name__)
+    conversation = ConvesationState()
+
+    @app.route("/msg")
+    def msg():
+        return "Hello, World!"
+
+    app.run()
